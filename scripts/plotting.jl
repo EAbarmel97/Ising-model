421fdc3e127a92d165ddee461b0479ec361c8d67
Enki Barra <enki.barmel97@ciencias.unam.mx>
2023-06-15 15:31:01 -0600

    adding of some simulations

--- scripts/plotting.jl
+++ scripts/plotting.jl
@@ -1,43 +1,42 @@
 #= Automation of global magnetization time series plot file saving at temperatures ranging from 1.0 to 3.5=#
-
 include("../scripts/src/graphTrazes.jl")
 using .graphTrazes: save_traze
-
-cd("../scripts") #change file pointer execution
+cd("..") #change file pointer execution
 
 #auxiliary file directory definitions
-file_dir_template1 = "simulations_T_1"
-file_dir_template2 = "simulations_T_2"
-file_dir_Tc = "simulations_T_2_269185314213020/magnetizaton/global_magnetization_r1.txt"
-file_dir_template3 = "simulations_T_3"
+curr_dir = pwd()
 
+file_dir_template1 = curr_dir * "/scripts/simulations_T_1"
+file_dir_template2 =  curr_dir * "/scripts/simulations_T_2"
+file_dir_Tc = curr_dir * "/scripts/simulations_T_2_269185314213020/magnetizaton/global_magnetization_r1.txt"
+file_dir_template3 = curr_dir *  "/scripts/simulations_T_3"
 
 for i in 0:9 #for temperatures in the interval 1.0-1.9 
     aux_dir = file_dir_template1 * "_$(i)" * "/magnetization/global_magnetization_r1.txt"
-    aux_graph_name = "graphs/" * "magn_ts_T_1_$(i).pdf"
-    if isdir(aux_dir)
+    aux_graph_name = curr_dir * "/graphs/magn_ts_T_1_$(i).pdf"
+    if isfile(aux_dir)
         graphTrazes.save_traze(aux_graph_name, aux_dir)
     end
 end
 
 for i in 0:9 #for temperatures in the interval 2.0-2.9
     aux_dir = file_dir_template2 * "_$i" * "/magnetization/global_magnetization_r1.txt"
-    aux_graph_name = "graphs/" * "magn_ts_T_1_$(i).pdf"
-    if isdir(aux_dir)
+    aux_graph_name = curr_dir * "/graphs/magn_ts_T_1_$(i).pdf"
+    if isfile(aux_dir)
         graphTrazes.save_traze(aux_graph_name, aux_dir)
     end
 end
 
 #at critical temperature
-if isdir(file_dir_Tc)
-    graphTrazes.save_traze("graphs/magn_ts_Tc.pdf", file_dir_Tc)
+if isfile(file_dir_Tc)
+    graph_dir = curr_dir * "/graphs/magn_ts_Tc.pdf"
+    graphTrazes.save_traze(graph_dir, file_dir_Tc)
 end
 
 for i in 0:5 #for temperatures in the interval 3.0-3.5
     aux_dir = file_dir_template3 * "_$(i)" * "/magnetization/global_magnetization_r1.txt"
-    aux_graph_name = "graphs/" * "magn_ts_T_1_$(i).pdf"
-    if isdir(aux_dir)
+    aux_graph_name = curr_dir * "/graphs/magn_ts_T_1_$(i).pdf"
+    if isfile(aux_dir)
         graphTrazes.save_traze(aux_graph_name, aux_dir)
     end
 end
-
