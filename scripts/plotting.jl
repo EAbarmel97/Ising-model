aba7b89d1cfad36dea10d9754e75c72a0cc6aa2b
Enki Barra M <enki.barmel97@ciencias.unam.mx>
2023-06-15 01:04:38 -0600

    modified:   scripts/plotting.jl

--- scripts/plotting.jl
+++ scripts/plotting.jl
@@ -3,7 +3,7 @@
 include("../scripts/src/graphTrazes.jl")
 using .graphTrazes: save_traze
 
-cd("../scripts")#
+cd("../scripts") #change file pointer execution
 
 #auxiliary file directory definitions
 file_dir_template1 = "simulations_T_1"
@@ -13,7 +13,7 @@ file_dir_template3 = "simulations_T_3"
 
 
 for i in 0:9 #for temperatures in the interval 1.0-1.9 
-    aux_dir *= file_dir_template1 * "_$(i)" * "/magnetization/global_magnetization_r1.txt"
+    aux_dir = file_dir_template1 * "_$(i)" * "/magnetization/global_magnetization_r1.txt"
     aux_graph_name = "graphs/" * "magn_ts_T_1_$(i).pdf"
     if isdir(aux_dir)
         graphTrazes.save_traze(aux_graph_name, aux_dir)
@@ -24,7 +24,7 @@ for i in 0:9 #for temperatures in the interval 2.0-2.9
     aux_dir = file_dir_template2 * "_$i" * "/magnetization/global_magnetization_r1.txt"
     aux_graph_name = "graphs/" * "magn_ts_T_1_$(i).pdf"
     if isdir(aux_dir)
-        graphTrazes.save_traze("", aux_dir)
+        graphTrazes.save_traze(aux_graph_name, aux_dir)
     end
 end
 
@@ -37,8 +37,7 @@ for i in 0:5 #for temperatures in the interval 3.0-3.5
     aux_dir = file_dir_template3 * "_$(i)" * "/magnetization/global_magnetization_r1.txt"
     aux_graph_name = "graphs/" * "magn_ts_T_1_$(i).pdf"
     if isdir(aux_dir)
-        graphTrazes.save_traze("", aux_dir)
+        graphTrazes.save_traze(aux_graph_name, aux_dir)
     end
 end
 
-graphTrazes.save_traze("graphs/magn_ts1.pdf", "scripts/simulations_T_1_0/magnetization/global_magnetization_r1.txt")
