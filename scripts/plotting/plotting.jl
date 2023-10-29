bf9fbd23f2271429fc6dc4200be9a2be78db6013
Enki A. Barra M <enki.barmel97@ciencias.unam.mx>
2023-10-29 00:51:27 -0600

    chore: changes in plotting.jl

--- scripts/plotting/plotting.jl
+++ scripts/plotting/plotting.jl
@@ -6,13 +6,13 @@ include("../src/utils/paths.jl")
 
 #auxiliary file directory definitions
 const AUTOMATED_SIMULS_DIR_NAMES = readdir(AUTOMATED_SIMULS_DIR)
-#= 
+
 #= file manipulations =#
 touch("median_magn_automated.txt") #creation of file containing all mean global magnetization
 file_to_write_over = "median_magn_automated.txt"
 
 #= plotting each temperature group and appending it over the file median_magn_automated.txt =#
-GraphTrazes.graph_and_write_over_file!(AUTOMATED_SIMULS_DIR_NAMES,AUTOMATED_SIMULS_DIR,file_to_write_over) =#
+GraphTrazes.graph_and_write_over_file!(AUTOMATED_SIMULS_DIR_NAMES,AUTOMATED_SIMULS_DIR,file_to_write_over)
 
 # ploting mean magnetization vs temperature
 GraphTrazes.plot_median_magn("median_magn_automated.txt", "median_magn_automated.pdf")
