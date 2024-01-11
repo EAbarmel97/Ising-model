4f5377c929b9810d49cd800e701a80aff70a1ab1
Enki A. Barra M <enki.barmel97@ciencias.unam.mx>
2024-01-11 15:24:15 -0600

    fix: svd applied to a centered data matrix

--- scripts/svd/svd_practice.jl
+++ scripts/svd/svd_practice.jl
@@ -1,5 +1,6 @@
 
 using LinearAlgebra
+using Statistics 
 using Plots
 
 include("../src/fourier/CorrelatedNoise.jl")
@@ -16,15 +17,21 @@ for i in 1:10
     push!(corr_noises_arr,corr_noise)
 end    
 
-@show M = hcat(corr_noises_arr...)' #matrix of time series produced from correlated noises 
-@show rank(M)
+M = hcat(corr_noises_arr...)' #matrix of time series produced from correlated noises 
+@show size(M)
+@show row_dim,col_dim = size(M)[1],size(M)[2]
 
-prop_vals = svd(M).S
+mean_by_observation = reshape(mean.(eachcol(M)),col_dim,1) #row vector with per-row mean 
+ones = reshape(fill(1,row_dim),row_dim,1) #col vector with ones 
+
+@show size(mean_by_observation), size(ones)
+centered_M = M - ones * mean_by_observation'
+
+prop_vals = svd(centered_M).S
+
 len = length(prop_vals)
-
 plt = plot!(x=collect(1:len),prop_vals)
 savefig(plt, "$(pwd())/scripts/svd/eigvals.pdf")
-
 
 
 
