b189b5bd23ba42d092de48accdb1078a189c2c76
Enki A. Barra M <enki.barmel97@ciencias.unam.mx>
2024-01-31 20:28:47 -0600

    chore: beta_beta_fit.txt added, eigenspectrum_beta_2_0.pdf

--- scripts/svd/eigenvals_plot.jl
+++ scripts/svd/eigenvals_plot.jl
@@ -3,9 +3,9 @@ using .SVD: create_ts_matrix, centralize_matrix, plot_eigen_spectrum, write_beta
 
 include("../src/utils/paths.jl")
 
-ts_matrix = SVD.create_ts_matrix(2.0;number_of_observations=1000,number_of_realizations=10000)
+ts_matrix = SVD.create_ts_matrix(2.0;number_of_observations=10000,number_of_realizations=10000)
 M = SVD.centralize_matrix(ts_matrix)
 SVD.plot_eigen_spectrum(AUTOMATED_EIGEN_SEPCTRUM_GRAPHS_DIR,M,2.0)
 
-write_beta_beta_fit(0.0,3.0,2;number_of_observations=100,number_of_realizations=100)
+write_beta_beta_fit(0.0,3.0,10;number_of_observations=1000,number_of_realizations=10000)
 plot_beta_beta_fit("beta_beta_fit.txt")
